77b9ae29b3f9928fcbebd722e19d3791d594c986
ran55920 <benmansourrania758@gmail.com>
2025-03-27 19:06:08 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,7 +16,23 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById(tabId).classList.add('active');
         });
     });
-    
+    let currentIndex = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.opacity = i === index ? "1" : "0";
+    });
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+showSlide(currentIndex);
+setInterval(nextSlide, 3000); // تغيير الصورة كل 3 ثوانٍ
+
     // إظهار/إخفاء كلمة المرور
     const showPasswordBtn = document.querySelector('.show-password');
     const passwordInput = document.getElementById('password');
